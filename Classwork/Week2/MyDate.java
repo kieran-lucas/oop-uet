7189fdb0f0bd2dbaf5a80661b66a62e5f514d7a7
Kieran Lucas <kieranlucas.work@gmail.com>
2026-03-06 05:28:54 +0700

    update

--- Classwork/Week2/MyDate.java
+++ Classwork/Week2/MyDate.java
@@ -1,15 +1,16 @@
 class MyDate {
-	private int year, mon, day;
+	private int year, month, day;
 	public int getYear() {
 		return year;
 	}
 	public void setYear(int y ) {
 		year = y;
 	}
-	public static void main(String[] args) {
-		MyDate d = new MyDate();
-		d.year = 2025;
-		d.setYear(2025);
-		System.out.println(d.getYear());
+	public MyDate() {
+		day = 1;
+		month = 1;
+		year = 2026;
+		System.out.println("A new date created");
 	}
+	
 }
--- Classwork/Week2/TestMyDate.java
+++ Classwork/Week2/TestMyDate.java
@@ -1,6 +1,4 @@
 public static void main(String[] args) {
 		MyDate d = new MyDate();
-//  	d.year = 2025;
-		d.setYear(2025);
-		System.out.println(d.getYear());
+	
 	}
