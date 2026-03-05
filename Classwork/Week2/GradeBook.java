8fecbb0fe354d26a20a1319d5402531e10ca352b
Kieran Lucas <kieranlucas.work@gmail.com>
2026-03-06 05:18:07 +0700

    update

--- Classwork/Week2/GradeBook.java
+++ Classwork/Week2/GradeBook.java
@@ -1,10 +1,14 @@
 public class GradeBook {
 	private String courseName;
+	public GradeBook(String name) {
+
+		courseName = name;
+	}
 	public void setCourseName(String name) {
 		courseName = name;
 	}
 	public String getCourseName() {
-		return CourseName;
+		return courseName;
 	}
 	public void displayMessage() {
 		System.out.println("Welcome to the Grade Book!");
--- Classwork/Week2/GradeBookTest.java
+++ Classwork/Week2/GradeBookTest.java
@@ -1,6 +1,6 @@
 public class GradeBookTest {
 	public static void main(String args[] ){
-		GradeBook myGradeBook = new GradeBook();
-		myGradeBook.displayMessage();
+		GradeBook myGradeBook = new GradeBook("OOP");
+		System.out.println(myGradeBook.getCourseName());
 	}
 }
