6eb4265a33fa6436e1d5d976597ac3111621d1f6
Kieran Lucas <kieranlucas.work@gmail.com>
2026-03-19 19:28:59 +0700

    update

---
+++ Classwork/Week4/Main5.java
@@ -0,0 +1,20 @@
+interface Action {
+	void moveTo(int x, int y);
+	void erase();
+	void draw();
+}
+class Circle1 implements Action {
+	int x, y, r;
+	Circle1(int _x, int _y, int _r) {
+
+	}
+	public void erase() {
+
+	}
+	public vod draw() {
+
+	}
+	public void moveTo(int x1, int y1) {
+		
+	}
+}
