0870b3cb384b58ca025d0128ee97d9bd0c2b41cf
Kieran Lucas <kieranlucas.work@gmail.com>
2026-03-26 02:25:27 +0700

    update

--- Classwork/Week5/Main6.java
+++ Classwork/Week5/Main6.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 public class Main6 {
 	public static void main(String[] args) {
 		
 		int[] a = {0, 1, 1, 2, 3, 5, 8};
 		int[] b = myCopy(a);
-		System.out.println(b);
+		System.out.println(Arrays.toString(b));
 	}
 	public static int[] myCopy(int[] a) {
 		int b[] = new int[a.length];
