b94e04e43873804b88a5dec2d2b1b43963368202
Kieran Lucas <kieranlucas.work@gmail.com>
2026-03-26 02:24:18 +0700

    update

---
+++ Classwork/Week5/Main6.java
@@ -0,0 +1,15 @@
+public class Main6 {
+	public static void main(String[] args) {
+		
+		int[] a = {0, 1, 1, 2, 3, 5, 8};
+		int[] b = myCopy(a);
+		System.out.println(b);
+	}
+	public static int[] myCopy(int[] a) {
+		int b[] = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			b[i] = a[i];
+		}
+		return b;
+	}
+}
