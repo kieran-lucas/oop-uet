d3d0b56ac1245332aad31aab757ad17bc0358dc7
Kieran Lucas <kieranlucas.work@gmail.com>
2026-03-26 02:58:32 +0700

    update

---
+++ Classwork/Week5/Main9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main9 {
+	public static void main(String[] args) {
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.remove("BMW");
+		cars.add("Mazda");
+		System.out.println(cars);
+		
+
+	}
+}
