8f9b7fa06b60547b0ac8063a710b22350a4d5d01
Kieran Lucas <kieranlucas.work@gmail.com>
2026-04-06 09:02:11 +0700

    update

---
+++ Classwork/Week6/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		Singleton obj1 = Singleton.getInstance();
+		System.out.println(obj1);
+		Singleton obj2 = Singleton.getInstance();
+		System.out.println(obj2);
+
+
+
+	}
+}
+class Singleton {
+	private static Singleton instance = null;
+	public static Singleton getInstance() {
+		if (instance == null) {
+			instance = new Singleton();
+		} 
+		return instance;
+	}
+}
